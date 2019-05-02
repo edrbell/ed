14d04eeed301f090cedaea85015915fb35a21642
edrbell <1dam.rbell@institutoserpis.org>
2019-05-02 12:29:34 +0200

    Proyecto iniciado, treeView

---
+++ pcsharp/pcsharp/MainWindow.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+        treeView.AppendColumn("Nombre", new CellRendererText(), "text", 0);
+        treeView.AppendColumn("Teléfono", new CellRendererText(), "text", 1);
+        ListStore listStore = new ListStore(typeof(string), typeof(string));
+        listStore.AppendValues("Rosa", "999999444");
+        listStore.AppendValues("Pepe", "666699444");
+        treeView.Model = listStore;
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+}
