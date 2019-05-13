427230321b9ec60f027605cdfea5041d8c5ae607
edrbell <1dam.rbell@institutoserpis.org>
2019-05-13 08:56:48 +0200

    Recuperado/Añadida toolbox

---
+++ pcsharp/pcsharp/pcsharp/MainWindow.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+        treeView.AppendColumn("Nombre", new CellRendererText(), "text", 0);
+        treeView.AppendColumn("Teléfono", new CellRendererText(), "text", 1);
+        ListStore listStore = new ListStore(typeof(string), typeof(string));
+        listStore.AppendValues("Rosa", "999999444");
+        listStore.AppendValues("Pepe", "666699444");
+        treeView.Model = listStore;
+
+        editAction.Sensitive = false;
+
+        newAction.Activated += (sender, e) =>
+        {
+            vbox1.Visible = true;
+            treeView.Sensitive = false;
+        };
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+}
